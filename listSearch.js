a5992b2070b30c29c2f541816f25dba680c56be4
czy52099 <czy52099@gmail.com>
2024-04-19 15:20:12 +0900

    文言など修正

--- listSearch.js
+++ listSearch.js
@@ -1,17 +1,16 @@
 /**
  * 再帰関数利用してリストの値を探す
  */
-function listStr(lst,str,idx=0) {
-    let i=idx;
-    if(str==lst[i]){
-        console.log(i+1+"番目の【"+str+"】でした。");
+function listStr(lst, str, idx = 0) {
+    let i = idx;
+    if (str == lst[i]) {
+        console.log("「" + str + "」は" + (i + 1) + "番目にある。");
         return;
     }
-    if(lst.length<=i+1){
+    if (lst.length <= i + 1) {
         console.log("見つかりませんでした。");
         return;
     }
     i++;
-    listStr(lst,str,i)
-    return;
+    listStr(lst, str, i);
 }
